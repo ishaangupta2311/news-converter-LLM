784c41c1e699fd29735a5f7bfa38310d14f4ce09
MachineMuggle <ishaangupta1104@gmail.com>
2024-11-07 23:58:41 +0530

    Create separate component for video-translation

---
+++ src/components/video-news-translation.tsx
@@ -0,0 +1,20 @@
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "./ui/card"
+import {Video} from 'lucide-react'
+
+const videoNewsTranslation = () => {
+  return (
+    <div>
+      <Card className="cursor-pointer hover:shadow-lg transition-shadow" onClick={() => handleOptionSelect('video')}>
+                <CardHeader>
+                  <CardTitle className="flex items-center justify-center">
+                    <Video className="h-8 w-8 mr-2 text-blue-600" />
+                    Translate News Video
+                  </CardTitle>
+                </CardHeader>
+                 <CardContent>
+                  <CardDescription>Generate multilingual subtitles for news videos</CardDescription>
+                </CardContent>
+              </Card></div>
+  )
+}
+export  default videoNewsTranslation;
